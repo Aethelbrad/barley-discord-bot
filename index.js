ca5c576b70ab49d39abf2a513402454d4b365af3
Marmacanna <a.stale.muffin@gmail.com>
2021-09-06 18:23:41 -0700

    implement api

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-// Require the necessary discord.js classes
-const { Client, Intents, MessageEmbed } = require('discord.js'); // MessageEmbed is the class that allows you to create embeds
-const { token } = require('./config.json');
+const { Client, Intents, MessageEmbed } = require("discord.js");
+const { token, APItoken } = require("./config.json");
+const axios = require("axios");
 const prefix = "!";
-const talkedRecently = new Set(); // Set is a collection of unique values
-// const intents = new Discord.Intents(32767); // 32767 is the value for all intents
 
-// Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] }); // intents: intents
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+}); // intents: intents
 
-// When the client is ready, run this code (only once)
-client.on('ready', () => { 
-    console.log( "\u001b[1;32m" + client.user.tag + " is online!" ); // ANSI color codes to work with VSC console. %c works in Chrome Debug.
-    client.user.setActivity( "How To Be A Better Bot", { type: "WATCHING" } );
+client.on("ready", () => {
+  console.log("\u001b[1;32m" + client.user.tag + " is online!"); // ANSI color codes to work with VSC console. %c works in Chrome Debug.
+  client.user.setActivity("How To Be A Better Bot", { type: "WATCHING" });
 });
 
-client.on('messageCreate', message => { 
-    // If the message doesn't start with the prefix, return. 
-    if (!message.content.startsWith(prefix)) return;
+// Event Listener for when a message is sent
+client.on("messageCreate", async message => {
+  if (!message.content.startsWith(prefix)) return;
 
-    const args = message.content.substring(prefix.length).split(/ +/); // args is an array of strings. substring(config.prefix.length) removes the prefix from the message. split(/ +/) splits the message into an array of strings. Essentially this line gets rid of spaces in messages and makes it an array of strings.
+  const args = message.content.slice(prefix.length).split(' ');
 
     switch (args[0]) {
-        case 'hello': message.reply("Hello!"); break; 
-        case 'say': message.reply(args.slice(1).join("")); break;
-        case 'w':
+        case "w":
+        await axios
+            .get(
+            `http://api.openweathermap.org/data/2.5/weather?q=${args}&units=metric&appid=${APItoken}`
+            )
+            .then((response) => {
+            const apiData = response;
             const embed = new MessageEmbed()
-                .setTitle("Weather")
-                .setDescription("It's raining!")
-                .setAuthor('Weather forcast for (current city)')
-                .addField("Temperature", "It's currently (temperature) degrees")
-                .addField("Wind", "It's currently (wind speed) mph")
-                .addField("Humidity", "It's currently (humidity)%")
-                .addField("Timezone", "It's currently (timezone)")
-                .setColor(0x00AE86)
-                .setFooter("Barley v0.1")
-            message.reply({ embeds: [embed] }); // this statement sends the embed to the channel. I should probably fin
-                break;
-    }
+                .setTitle(`${args}`)
+                .setDescription(`${Math.ceil(apiData.data.main.temp)} degrees Fahrenheit`)
+                .addField("Humidity", `${apiData.data.main.humidity}%`)
+                message.channel.send({ embeds: [embed] });
+                console.log(response);
+            })
+            .catch((err) => {
+            console.log(err);
+            });
+        break;
+    };
 });
-
-// Login to Discord with your client's token
 client.login(token);
