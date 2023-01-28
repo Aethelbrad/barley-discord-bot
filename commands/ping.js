9b04042b402b12eb1a0620dc56109fdb15b36318
Aethelbrad <a.stale.muffin@gmail.com>
2023-01-28 07:37:24 -0800

    added bot uptime to ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -8,7 +8,8 @@ module.exports = {
     await interaction.reply(
       `Server name: ${interaction.guild.name}
 	  \nTotal members: ${interaction.guild.memberCount}
-	  \nServer created: ${interaction.guild.createdAt.toLocaleString()}`
+	  \nServer created: ${interaction.guild.createdAt.toLocaleString()}
+    \nBarley Uptime: ${process.uptime().toLocaleString()} seconds, or ${( process.uptime() / 60 ).toLocaleString()} minutes, or ${( process.uptime() / 3600 ).toLocaleString()} hours, or ${( process.uptime() / 86400 ).toLocaleString()} days`
     );
   },
 };
